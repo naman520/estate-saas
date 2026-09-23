0c18a0c53394d3fdd7d184b0feb6e0cfd083d6e4
Naman <namangupta2360@gmail.com>
2026-09-23 22:36:20 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,25 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-<<<<<<< HEAD
   allowedDevOrigins: [
     "https://postwar-daybreak-theology.ngrok-free.dev",
     "http://esatets.test"
   ],
-=======
-  allowedDevOrigins:[
-    "https://postwar-daybreak-theology.ngrok-free.dev"
-  ],   
-async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/dashboard",
-        permanent: false, // 307 redirect
-      },
-    ];
-  },
->>>>>>> 8a7656b51bbfd3b2e14c9060d491551422e3bbe6
 };
 
 export default nextConfig;
